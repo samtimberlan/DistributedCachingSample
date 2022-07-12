bcbceaa3829e98d47c434d8df7f8ad750e92709c
Samtimberlan <timudoma@gmail.com>
2022-07-11 19:55:29 -0700

    Add project files.

---
+++ DistributedCaching Sample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using DistributedCaching_Sample.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistributedCaching_Sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IDistributedCache _cache;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            // Check if content exists in cache
+            WeatherForecast[] weatherResult = await _cache.GetCacheValueAsync<WeatherForecast[]>("weatherResult");
+            if (weatherResult != null)
+            {
+                return weatherResult;
+            }
+            var rng = new Random();
+            weatherResult = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+                .ToArray();
+
+            await _cache.SetCacheValueAsync("weatherResult", weatherResult);
+            return weatherResult;
+        }
+    }
+}
---
+++ DistributedCaching Sample/Extensions/Cache Extensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace DistributedCaching_Sample.Extensions
+{
+    public static class CacheExtensions
+    {
+        public static string ToJsonString(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static async Task<T> GetCacheValueAsync<T>(this IDistributedCache cache, string key) where T : class
+        {
+            string result = await cache.GetStringAsync(key);
+            if (String.IsNullOrEmpty(result))
+            {
+                return null;
+            }
+            var deserializedObj = JsonConvert.DeserializeObject<T>(result);
+            return deserializedObj;
+        }
+
+        public static async Task SetCacheValueAsync<T>(this IDistributedCache cache, string key, T value) where T : class
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new();
+
+            // Remove item from cache after duration
+            cacheEntryOptions.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(60);
+
+            // Remove item from cache if unsued for the duration
+            cacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(30);
+
+            string result = value.ToJsonString();
+
+            await cache.SetStringAsync(key, result);
+        }
+    }
+}
